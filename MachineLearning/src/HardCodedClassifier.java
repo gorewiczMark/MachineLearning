7a19b6e0e76a5a5bb0f35cdd7922d9da81008d58
GorewiczM <gorewicz.mark@gmail.com>
2015-06-01 15:11:42 -0600

    renamed files

---
+++ MachineLearning/src/HardCodedClassifier.java
@@ -0,0 +1,29 @@
+import weka.classifiers.*;
+import weka.classifiers.trees.J48;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.converters.CSVLoader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.instance.*;
+
+import java.io.IOException;
+import java.io.File;
+
+@SuppressWarnings("serial")
+public class HardCodedClassifier extends AbstractClassifier
+{   
+	@Override
+	public void buildClassifier(Instances data) throws Exception 
+	{
+		return;	
+	}	
+	
+	@Override
+	public double classifyInstance(Instance data)
+	{
+		return 0;
+	}
+	
+}
